ae470282906a3d0356676ba1c95f55f7471100d6
lathiesh24 <lathumahi24@gmail.com>
2023-08-02 11:48:49 +0530

    recent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+
+   <div className="App"> 
+   <Header/>
+     <Routes>
+       <Route exact path="/"  element={<Home/>}/>
+       <Route path="/cart"  element={<Cart/>}/>
+     </Routes>
+   </div>      
   );
 }
 
